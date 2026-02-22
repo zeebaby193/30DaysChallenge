0936eab34e049be0a0c6c11a2d7a458bcef9ea78
zeebaby193 <zainabenitan422@gmail.com>
2026-02-22 23:16:52 +0100

    Day1 initial commit

---
+++ Day1/Program.cs
@@ -0,0 +1,13 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+//This program is welcoming you to programming and telling you her name and best food
+Console.WriteLine("Welcome to Programming");
+Console.WriteLine("My name is Henny.");
+Console.WriteLine("My best food is Snacks and Crunches.");
+
+//Q. write a program that prints your name, age and state using coorect syntax and comment.
+Console.WriteLine("My name is Opaleke Zainab.");
+Console.WriteLine("I am 12 years old.");
+Console.WriteLine("I am from Osun State.");
+//This program is showcasing the name, age and state of the user.
