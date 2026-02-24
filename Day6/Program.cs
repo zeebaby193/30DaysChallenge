be77983e25cc3d2810a69651d471805a7936eed2
zeebaby193 <zainabenitan422@gmail.com>
2026-02-24 14:45:02 +0100

    Add Day6 project

---
+++ Day6/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+Console.Write("Input firstNumber: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+
+Console.Write("Input secondNumber: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(firstNumber + secondNumber);
+Console.WriteLine(firstNumber - secondNumber);
+Console.WriteLine(firstNumber * secondNumber);
+Console.WriteLine(firstNumber / secondNumber);
+
+
+
+Console.Write("Input a number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(number * number);
+Console.WriteLine(number * number * number);
+
+
+
+Console.Write("Input total marks: ");
+double totalMarks = double.Parse(Console.ReadLine()!);
+
+Console.Write("Input number of subjects: ");
+int numberOfSubjects = int.Parse(Console.ReadLine()!);
+
+double averageMarks = totalMarks / numberOfSubjects;
+Console.WriteLine($"Average marks: {averageMarks}");
+
+
+
+
