a5de9503f905915d5e1eb7761a0a09a19a126dd7
zeebaby193 <zainabenitan422@gmail.com>
2026-02-25 15:02:37 +0100

    Add Day7 Project

---
+++ Day7/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+// string name = "Zain Henny";
+
+// int charPos = name.IndexOf("Z");
+
+// string firstName = name.Substring(charPos);
+
+// Console.WriteLine(firstName);
+
+// int myAge = 25;
+// int votingAge = 18;
+// Console.WriteLine(myAge >= votingAge);
+
+
+// string name = "Henny";
+// bool isProgrammer = true;
+// Console.WriteLine(name + " is learning C#: " + isProgrammer);
+
+
+string name = "Henny";
+int age = 12;
+bool isLearning = true;
+
+Console.WriteLine($"My name is {name}, I am {age} years old, And am learning C#: {isLearning}");
+
+string username = "Henny";
+string password = "1234";  
+bool isLoggedIn = false;
+
+if (password == "1234")
+{
+    isLoggedIn = true;
+}
+
+Console.WriteLine($"Username: {username}");
+Console.WriteLine($"Password: {password}");
+Console.WriteLine($"Login successful: {isLoggedIn}");
+
+string studentNmae = "Zain";
+int examScore = 60;
+bool isPass = false;
+
+if (examScore >= 40)
+{
+    isPass = true;
+}
+
+
+Console.WriteLine($"studentName: {studentNmae}");
+Console.WriteLine($"examScore: {examScore}");
+Console.WriteLine($"You Passed: {isPass}");
