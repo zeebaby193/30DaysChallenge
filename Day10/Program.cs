3fbadb26fc558e91802f9f2b957e4186412a0ec3
zeebaby193 <zainabenitan422@gmail.com>
2026-02-27 15:18:03 +0100

    Add Day10 Project

---
+++ Day10/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int i = 1;
+while (i <= 10)
+{
+    Console.WriteLine(i);
+    i++;
+}
+
+int m = 2;
+while (m <= 20)
+{
+    Console.WriteLine(m);
+    m += 2;
+}
+
+int secretNumber = 7;
+int userGuess = 0;
+while (userGuess != secretNumber)
+{
+    Console.Write("Guess the secret number: ");
+    userGuess = int.Parse(Console.ReadLine()!); 
+}
+Console.WriteLine("Correct!");  
+
+
+
