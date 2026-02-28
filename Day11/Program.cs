1434049d52d8f2e8b72c59d60ac7c01236dc8dc3
zeebaby193 <zainabenitan422@gmail.com>
2026-02-28 11:22:07 +0100

    Add Day11 Project

---
+++ Day11/Program.cs
@@ -0,0 +1,30 @@
+﻿
+for (int i = 1; i <= 20; i++)
+{
+    Console.WriteLine(i);
+}
+
+
+for (int r = 5; r <= 50; r += 5)
+{
+    Console.WriteLine(r);
+}
+
+for (int l = 5; l > 0; l--)
+{
+    Console.WriteLine(l);
+}
+
+for (int m = 50; m >= 5; m = m - 5)
+{
+    Console.WriteLine(m);
+}
+
+Console.Write("Input number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+for (int i = 1; i <= 12; i++)
+{
+    Console.WriteLine(number * i);
+}
+
