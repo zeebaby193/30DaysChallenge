4fdf4aeff27bcc29d50aba485fd1b42090702241
zeebaby193 <zainabenitan422@gmail.com>
2026-03-04 14:38:58 +0100

    Add Day13 Project

---
+++ Day13/Program.cs
@@ -0,0 +1,53 @@
+﻿
+for (int i = 1; i <= 10; i++)
+{
+    if (i == 5)
+    {
+        break;
+    }
+    Console.WriteLine(i);
+}
+
+for (int i =1; i <= 5; i++)
+{
+    if (i == 3)
+    {
+        continue;
+    }
+    Console.WriteLine(i);
+}
+
+for (int i = 1; i <= 10; i++)
+{
+    if (i % 2 != 0)
+    {
+        continue;
+    }
+    Console.WriteLine(i);
+}
+
+for (int i = 1; i <= 6; i++)
+{
+    if (i == 3)
+    {
+        continue;
+    }
+    if (i % 2 == 0)
+    {
+        break;
+    }
+    Console.WriteLine(i);
+}
+
+for (int i =1; i <= 6; i++)
+{
+    if (i % 2 == 0)
+    {
+        continue;
+    }
+    if (i == 5)
+    {
+        break;
+    }
+    Console.WriteLine(i);
+}
