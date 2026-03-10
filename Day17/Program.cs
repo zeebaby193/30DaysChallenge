ff47c8a29c82471401f7d9242173c52159994674
zeebaby193 <zainabenitan422@gmail.com>
2026-03-10 11:53:57 +0100

    Add Day17 Project

---
+++ Day17/Program.cs
@@ -0,0 +1,65 @@
+﻿
+static void AddNumber(int a, int b)
+{
+    Console.WriteLine(a + b);
+}
+AddNumber(5, 3);
+
+static void StudentInfo(string name, int age)
+{
+    Console.WriteLine(name + " is " + age + " years old ");
+}
+StudentInfo("Henny", 16);
+
+
+GreetUser("Henny");
+AddNumbers(4, 6);
+static void GreetUser(string name)
+{
+    Console.WriteLine("Hello " + name);
+}
+
+static void AddNumbers(int a, int b)
+{
+    Console.WriteLine("Sum: " + (a + b));
+}
+
+static void PrintNumber(int num)
+{
+    Console.WriteLine(num);
+}
+PrintNumber(7);
+
+static void MultiplyNumber(int a, int b)
+{
+    Console.WriteLine("Product: " + (a * b));
+}
+MultiplyNumber(4, 5);
+
+static void Student(string name, int score)
+{
+    Console.WriteLine("StudentInfo: " + ("My name is " + name + " I have: " + score , "mark"));
+
+    if (score >= 50)
+    {
+        Console.WriteLine("Henny Passed!");
+    }
+    else
+    {
+        Console.WriteLine("Henny Failed!");
+    }
+}
+Student("Henny", 40);
+
+static void ShowNumber(int number)
+{
+    Console.WriteLine(number + 5);
+}
+ShowNumber(10);
+
+static void CalculateArea(int length, int width)
+{
+    Console.WriteLine("Area: " + (length * width));
+}
+CalculateArea(5, 4);
+
